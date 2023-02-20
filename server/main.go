9fe0e655b0f3f84baf120c1207289199e37fc2a1
hoyeoun <jay0i8111@naver.com>
2023-02-20 22:31:11 +0900

    # ADD - echo add

--- server/main.go
+++ server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Todo struct {
+	Id    int    `json:"id"`
+	Done  bool   `json:"done"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
 
 func main() {
 	fmt.Println("hello world!")
+	e := echo.New()
+
+	e.GET("/todo", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":4000"))
+}
+
+func GetTodo(c echo.Context) {
 }
